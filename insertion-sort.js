be4e773bff727e776f0c1396b369ea58af154b1e
Benjamin Habermehl <75319001@edu.cbe.ab.ca>
2022-09-13 10:03:15 -0600

    done

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,4 +1,13 @@
 function insertionSort(array) {
+	for(i = 1; i < array.length; i++){
+		let current = array[i]
+		let j = i-1;
+		while((j > -1) && (current < array[j])){
+			array[j + 1] = array[j];
+			j--;
+		}	
+		array[j+1] = current
+	}
 	/*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
